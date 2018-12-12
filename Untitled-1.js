31e31fffafea28d85ab4037884a69ff79848cdfb
Kevin Edmondson <kevinedmondson@Kevins-MacBook-Pro.local>
2018-12-11 19:12:19 -0600

    first-change

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+let firstName = 'Judy'
+firstName 
+let lastName = 'Sandoval'
+let fullName = (firstName + " " + lastName);
+fullName
+let happy = function() {
+    let happyBoy = (fullName + " is really happpppy!");
+    return happyBoy;
+}
+console.log(happy());
